db3662d811ce1d0ce93e46a56651df312ded19ac
perdomofranklindev <perdomofranklindev@gmail.com>
2023-06-09 14:28:52 -0400

    code cleaned

--- src/constants/notification.ts
+++ src/constants/notification.ts
@@ -1,19 +1,19 @@
 export const NOTIFICATION_TYPES = {
   order: {
-    reminder: {
+    REMINDER: {
       title: 'Reminder',
       content: (time: string): string =>
         `You have an order that most be delivered at ${time}`,
     },
-    assignment: {
+    ORDER_ASSIGNMENT: {
       title: 'Order Assignment',
       content: 'A new order was assigned to you',
     },
-    new: {
+    NEW_ORDER: {
       title: 'New Order',
       content: 'There is a new order available for pick-up',
     },
-    problem: {
+    ORDER_PROBLEM: {
       title: 'Order Problem',
       content: 'There has been a problem with the delivery of the order',
     },
